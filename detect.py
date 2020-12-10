d6381f113da99b4f4f4cb3d3a6244c36a3a49989
David Relke <david.relke@stud.tu-darmstadt.de>
2020-12-10 23:23:23 +0100

    Use same color for class on each image

--- detect.py
+++ detect.py
@@ -28,11 +28,12 @@ if __name__ == "__main__":
     parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/carla.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
     parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-    parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
+    parser.add_argument("--draw_labels", action="store_true")
+    parser.add_argument("--out", type=str, default=None)
     opt = parser.parse_args()
     print(opt)
 
@@ -107,9 +108,9 @@ if __name__ == "__main__":
         if detections is not None:
             # Rescale boxes to original image
             detections = rescale_boxes(detections, opt.img_size, img.shape[:2])
-            unique_labels = detections[:, -1].cpu().unique()
-            n_cls_preds = len(unique_labels)
-            bbox_colors = random.sample(colors, n_cls_preds)
+            # unique_labels = detections[:, -1].cpu().unique()
+            # n_cls_preds = len(unique_labels)
+            # bbox_colors = random.sample(colors, n_cls_preds)
             for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
 
                 print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
@@ -117,25 +118,32 @@ if __name__ == "__main__":
                 box_w = x2 - x1
                 box_h = y2 - y1
 
-                color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+                # color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+                color = colors[int(cls_pred) * 2]
                 # Create a Rectangle patch
                 bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
                 # Add the bbox to the plot
                 ax.add_patch(bbox)
                 # Add label
-                # plt.text(
-                #     x1,
-                #     y1,
-                #     s=classes[int(cls_pred)],
-                #     color="white",
-                #     verticalalignment="top",
-                #     bbox={"color": color, "pad": 0},
-                # )
+                if opt.draw_labels:
+                    plt.text(
+                        x1,
+                        y1,
+                        s=classes[int(cls_pred)],
+                        color="white",
+                        verticalalignment="top",
+                        bbox={"color": color, "pad": 0},
+                        fontsize=6
+                    )
 
         # Save generated image with detections
         plt.axis("off")
         plt.gca().xaxis.set_major_locator(NullLocator())
         plt.gca().yaxis.set_major_locator(NullLocator())
-        filename = path.split("/")[-1].split(".")[0]
-        plt.savefig(f"output/{filename}.png", bbox_inches="tight", pad_inches=0.0)
+        filename = path.split("/")[-1].split(".")[0].replace("\\", "/")
+        out_dir = "output" if opt.out is None else opt.out
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        file_name = f"{out_dir}/{filename.split('/')[-1]}.png"
+        plt.savefig(file_name, bbox_inches="tight", pad_inches=0.0)
         plt.close()
