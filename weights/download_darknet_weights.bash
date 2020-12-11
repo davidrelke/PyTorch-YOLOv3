b04c563af45483b5967337750270beb5d0b45449
David Relke <david.relke@stud.tu-darmstadt.de>
2020-12-11 21:15:11 +0100

    Update weight download scripts

--- weights/download_darknet_weights.bash
+++ weights/download_darknet_weights.bash
@@ -1,6 +1,6 @@
-# https://drive.google.com/file/d/1T3OBV6CLDNG7Nm9ElLZJqQ7oLmFWnNWS/view?usp=sharing
-fileId=1T3OBV6CLDNG7Nm9ElLZJqQ7oLmFWnNWS
-fileName=yolov3-carla-30.pth
+# https://drive.google.com/file/d/1OrYrD0VzLjq9xjvtOF371QZ1F4FTuwH5/view?usp=sharing
+fileId=1OrYrD0VzLjq9xjvtOF371QZ1F4FTuwH5
+fileName=darknet53.conv.74
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
